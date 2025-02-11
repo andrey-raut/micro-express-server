8a8ac2dcd0dcaf539789e40967a83a1c0d7ba19e
andrey_raut <andrey.raut@fusion-tech.pro>
2025-02-11 22:41:15 +0300

    Extend base app configuration

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
 
 const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: '*' }));
+
+const staticDirPath = path.normalize(`${__dirname}/../public`);
+app.use('/public', express.static(staticDirPath));
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World!' });
